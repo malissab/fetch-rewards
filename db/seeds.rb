1095bd2e31cfc9dcb8532d621c3317a19ff711f1
malissab <malissambishop7@gmail.com>
2022-05-03 17:45:18 -0700

    mvc created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'seeding'
+
+Transaction.create ([
+{ "payer": "DANNON", "points": 1000, "timestamp": "2020-11-02T14:00:00Z" },
+{ "payer": "UNILEVER", "points": 200, "timestamp": "2020-10-31T11:00:00Z" },
+{ "payer": "MILLER COORS", "points": 10000, "timestamp": "2020-11-01T14:00:00Z" },
+{ "payer": "DANNON", "points": 300, "timestamp": "2020-10-31T10:00:00Z" }
+])
+
+puts 'seeded!'
